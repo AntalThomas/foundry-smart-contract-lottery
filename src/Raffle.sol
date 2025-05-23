5494536199a1bf824f5199fe62df7ada73cafbc8
AntalThomas <Blake-Heward@hotmail.com>
2025-05-23 15:31:09 +1000

    First Commit

---
+++ src/Raffle.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+    @title Raffle
+    @author Blake Varbai-Heward
+    @notice This contract is a simple implementation of a raffle system.
+    @dev The contract allows users to enter a raffle and randomly selects a winner.
+*/
+contract Raffle {
+
+}
